6dddfbfc5301bbed1489809e788a4b23b0b84da7
rizkiismail9a <muhamadrizkiismail9a@gmail.com>
2026-08-09 21:02:48 +0700

    feat: create footer

---
+++ app/components/Footer.vue
@@ -0,0 +1,21 @@
+<script setup></script>
+
+<template>
+  <footer>
+    <p>Dibuat dengan penuh kesungguhan &copy; Muhamad Rizki Ismail 2026</p>
+  </footer>
+</template>
+
+<style lang="scss" scoped>
+footer {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 24px;
+
+  p {
+    font-size: 14px;
+    letter-spacing: 0.2px;
+  }
+}
+</style>
